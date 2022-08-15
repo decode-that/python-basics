7c308cd704400e7a6a46a1b006bcc542452ffe2b
Kes <kestutis.k@posteo.net>
2022-08-15 22:44:11 +0200

    [update] integer into a string / 3 methods

---
+++ snippets/python-basics-integer-string.py
@@ -0,0 +1,10 @@
+# Inserting Integer into a String
+# 3 Methods
+no_of_bees = 2
+
+# Method 1: %
+print('My dog stepped on %s bees' %(no_of_bees))
+# Method 2: .format()
+print('My dog stepped on {} bees'.format(no_of_bees))
+# Method 3: f-String
+print(f"My dog stepped on {no_of_bees} bees")
